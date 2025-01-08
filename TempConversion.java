00c49aca7a1501de745ce2a0f2682ffefa33fd7b
R-scan1 <deepr7590@gmail.com>
2025-01-08 10:22:44 +0530

    Third Program DAY1

---
+++ TempConversion.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+class TempConversion{
+    public static void main(String args[]){
+	   Scanner sc=new Scanner(System.in);
+	   int a=sc.nextInt();
+	   int f=(a*9/5)+32;
+	   System.out.println(f);
+	}
+}
