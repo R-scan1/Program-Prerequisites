c252fb71a214e617bb8b71199f4d2a392b606bb8
R-scan1 <deepr7590@gmail.com>
2025-01-08 10:34:46 +0530

    Updated the Third Code

--- TempConversion.java
+++ TempConversion.java
@@ -1,10 +1,11 @@
-import java.util.*;
+import java.util.Scanner;
 
-class TempConversion{
-    public static void main(String args[]){
-	   Scanner sc=new Scanner(System.in);
-	   int a=sc.nextInt();
-	   int f=(a*9/5)+32;
-	   System.out.println(f);
-	}
+public class TempConversion {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter temperature in Celsius: ");
+        double celsius = scanner.nextDouble();
+        double fahrenheit = (celsius * 9 / 5) + 32;
+        System.out.println("Temperature in Fahrenheit: " + fahrenheit);
+    }
 }
